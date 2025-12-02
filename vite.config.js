43efa39a500b9d41d1d7a7b0149357fb286f3ac6
James Michael Hunter Turner <michael@turneratlanta.com>
2025-12-02 13:21:48 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
+    build: {
     outDir: 'dist',
-  },
+    },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "./src/scss/main.scss";`
+      }
+    }
+  }
 });
